0130c0516b4a4debff1f4233fe6730913f2c0f51
Alexis Marquez <alexismarq337@gmail.com>
2023-08-09 13:01:15 -0600

    Added basic CRUD functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Bussiness = require('./models/Businesses'); //name of model: Bussiness
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://127.0.0.1:27017/reviews',{ //name of database: reviews
     useNewUrlParser: true,
@@ -18,10 +19,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-
-app.listen(3000, ()=>{
-    
-})
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req, res)=>{
     res.render('home');
@@ -29,4 +28,39 @@ app.get('/',(req, res)=>{
 app.get('/Places',async (req, res)=>{ //Index page
     const places = await Bussiness.find({}); //name of model: Bussiness
     res.render('Places/index', {places});
+})
+
+app.get('/Places/new',(req, res)=>{
+    res.render('Places/new')
+})
+
+app.post('/Places', async(req,res)=>{
+    const place = new Bussiness(req.body.bussiness)
+    await place.save();
+    res.redirect(`/Places/${place.id}`);
+})
+app.get('/Places/:id/edit',async(req, res)=>{
+    const place = await Bussiness.findById(req.params.id)
+    res.render('Places/edit', {place}) 
+})
+
+app.put('/places/:id',async(req,res)=>{
+    const{id}=req.params;
+    const place = await Bussiness.findByIdAndUpdate(id, {...req.body.bussiness})
+    res.redirect(`/Places/${place.id}`);
+})
+
+app.delete('/places/:id', async(req, res)=>{
+    const {id} = req.params;
+    const place = await Bussiness.findByIdAndDelete(id);
+    res.redirect('/Places');
+})
+
+app.get('/Places/:id', async(req, res)=>{
+    const place = await Bussiness.findById(req.params.id)
+    res.render('Places/show', {place})
+})
+
+app.listen(3000, ()=>{
+    
 })
